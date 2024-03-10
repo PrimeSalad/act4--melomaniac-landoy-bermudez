41dd5cb5957a661a5846b3731f462436d8087862
LandoyGeneElpie <g.landoyelpie@gmail.com>
2024-03-10 22:17:56 +0800

    song6.php added

---
+++ song6.php
@@ -0,0 +1,122 @@
+<?php
+
+function centerText($text) {
+    $line_length = strlen($text);
+    $padding = str_repeat(" ", (100 - $line_length) / 2);
+    return $padding . $text . $padding;
+}
+
+echo "<h1 style='font-size: 24px; text-align: center;'>Cardigan</h1>";
+
+echo "<p style='font-size: 12px; text-align: center;'>by Taylor Swift<br><br></p>";
+
+echo "<div style='text-align: center;'>";
+
+echo "<span style='font-size: 14px;'>[Verse 1]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Em                          A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Vintage tee, brand new phone</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                         G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>High heels on cobblestones</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                          A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>When you are young they assume you know nothing</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Em                          A                  G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Sequined smile, black lipstick, sensual politics</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                            A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>When you are young, they assume you know nothing</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Pre-Chorus]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>D                                  A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>But I knew you, dancin' in your Levi's</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Drunk under a streetlight, I</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                             A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>I knew you, hand under my sweatshirt</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> </span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>   Baby, kiss it better, I</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Chorus 1]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Em                                   A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And when I felt like I was an old cardigan</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                       G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Under someone's bed</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                     A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You put me on and said I was your favorite</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Verse 2]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Em                                 A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>A friend to all is a friend to none</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                             G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Chase two girls, lose the one</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                            A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>When you are young, they assume you know nothing</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Pre-Chorus]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>D</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>But I knew you</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                    A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Playing hide-and-seek and</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                            G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Giving me your weekends, I</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> </span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>   I knew you</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                         A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Your heartbeat on the High Line</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> </span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>   Once in twenty lifetimes, I</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Chorus 2]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>G                                    Bm</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And when I felt like I was an old cardigan</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                       D</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Under someone's bed</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                     A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You put me on and said I was your favorite</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Break]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>G   Bm</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>A   G</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Verse 3]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>To kiss in cars, and downtown bars</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Was all we needed</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You drew stars around my scars</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>But now I'm bleedin'</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> 'Cause I knew you</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                   A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Steppin' on the last train</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Marked me like a bloodstain, I</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>I knew you</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                       A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Tried to change the ending</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Peter losin' Wendy, I</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>I knew you</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                   A                             G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Leavin' like a father, runnin' like water, I</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                            A</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>When you are young, they assume you know nothing</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Bridge]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>But I knew you'd linger like a tattoo kiss</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>I knew you'd haunt all of my what-ifs</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>The smell of smoke would hang around this long</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>'Cause I knew everything when I was young</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>I knew I'd curse you for the longest time</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Chasin' shadows in the grocery line</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>I knew you'd miss me once the thrill expired</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And you'd be standin' in my front porch light</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And I knew you'd come back to me</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And you'd come back to me</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And you'd come back to me</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And you'd come back</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Chorus 3]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And when I felt like I was an old cardigan</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                       Bm</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Under someone's bed</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>                                     D</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You put me on and said I was your favorite</span><br><br>";
+
+echo "</div>";
+
+?>
