1fbb3c3ef3fd68ddccfe6293444cf98079c37926
LandoyGeneElpie <g.landoyelpie@gmail.com>
2024-03-10 22:22:03 +0800

    song10.php

---
+++ song10.php
@@ -0,0 +1,120 @@
+<?php
+
+function centerText($text) {
+    $line_length = strlen($text);
+    $padding = str_repeat(" ", (100 - $line_length) / 2);
+    return $padding . $text . $padding;
+}
+
+echo "<h1 style='font-size: 24px; text-align: center;'>Bad Blood</h1>";
+echo "<p style='font-size: 12px; text-align: center;'>by Taylor Swift<br><br></p>";
+
+echo "<div style='text-align: center;'>";
+
+echo "<span style='font-size: 14px;'>[Intro]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            F               C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Cause baby now we got bad blood</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You know it used to be mad love</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            F                  C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>So take a look at what you've done</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Cause baby now we got bad blood, hey!</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F              C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Now we got problems</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>              G                 Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And I don't think we can solve them</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>             F          C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You made a really deep cut</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>          G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And baby now we got bad blood, hey!</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Verse]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F       C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Did you have to do this?</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>G                   Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>I was thinking that you could be trusted</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F       C            G              Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Did you have to ruin what was shiny now it's all rusted</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F       C               G                Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Did you have to hit me where I'm weak baby I couldn't breathe</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>     F      C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And rub it in so deep</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> G                            Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Salt in the wound like you're laughing right at me</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Pre-Chorus]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>F      C      G      Am              F</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Oh, it's so sad to think about the good times</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>C      G</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You and I</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Chorus]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            F               C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Cause baby now we got bad blood</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You know it used to be mad love</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            F                  C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>So take a look at what you've done</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Cause baby now we got bad blood, hey!</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F              C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Now we got problems</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>              G                 Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And I don't think we can solve them</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>             F          C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You made a really deep cut</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>          G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And baby now we got bad blood, hey!</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Bridge]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F                   C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Band-aids don't fix bullet holes</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> G             Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You say sorry just for show</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>        F                   C                G      Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>If you live like that, you live with ghosts</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F                   C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Band-aids don't fix bullet holes</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>  G            Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You say sorry just to show</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>        F                   C                G      Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>If you live like that, you live with ghosts</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>If you love like that blood runs cold</span><br><br>";
+
+echo "<span style='font-size: 14px;'>[Chorus]</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>           F               C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Cause baby now we got bad blood</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You know it used to be mad love</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            F                  C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>So take a look at what you've done</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Cause baby now we got bad blood, hey!</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F              C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Now we got problems</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>              G                 Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And I don't think we can solve them</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>             F          C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You made a really deep cut</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>          G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And baby now we got bad blood, hey!</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>             F               C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Cause baby now we got bad blood</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You know it used to be mad love</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            F                  C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>So take a look at what you've done</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>            G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Cause baby now we got bad blood, hey!</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'> F              C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>Now we got problems</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>              G                 Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And I don't think we can solve them</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>             F          C</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>You made a really deep cut</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>          G               Am</span><br>";
+echo "<span style='font-family: Arial, sans-serif;'>And baby now we got bad blood, hey!</span>";
+
+?>
